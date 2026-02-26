49a3a4bbed0c0d0392641e24b76d2e6e739da50c
binhb2111831 <binhhuynh190317@gmail.com>
2026-02-26 17:30:58 +0700

    20.9.0

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -9,5 +9,8 @@ export async function generateMetadata({ params }: Params) {
 }
 
 export default function Page({ params }: Params) {
-  return <><h1>Slug: {params.slug}</h1><p>HTB</p></>;
+  return <><h1>Slug: {params.slug}</h1>
+  <p>Hello World</p>
+  <p>HTB</p>
+  </>;
 }
